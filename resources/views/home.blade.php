49981cef2165c8386efc6153c4ea9daae3f7abec
Nicolo Valenza <nico.vale.spazio@gmail.com>
2026-08-26 17:59:27 +0200

    fix posts page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-  <section class="homepage">
-    <div class="homepage-content">
-      <div class="title">{{ get_bloginfo('name') }}</div>
-      <div class="home-p">
-        <p class="home-p2">{{ get_bloginfo('description') }}</p>
-      </div>
-    </div>
-
-    <section class="news-section">
-      <div class="news-header">
-        <div class="news-header-title">News</div>
-      </div>
-      <div class="news-content">
-        <div class="news-list">
-          @while(have_posts())
-            @php(the_post())
-            @include('components.post-card')
-          @endwhile
+  <section class="articles-page">
+    <div class="articles-container">
+      <div class="news-section news-section--full">
+        <div class="news-header">
+          <div class="news-header-title">News</div>
+        </div>
+        <div class="news-content">
+          <div class="news-list">
+            @while(have_posts())
+              @php(the_post())
+              @include('components.post-card')
+            @endwhile
+          </div>
         </div>
       </div>
-    </section>
+    </div>
   </section>
 @endsection
